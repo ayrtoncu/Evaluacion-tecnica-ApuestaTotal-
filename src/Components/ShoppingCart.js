5df977913d3c2fe2ecad4c5f9e6393e792ebc365
ayrtoncu <bc.ayrton@gmail.com>
2022-09-07 09:58:41 -0500

    #2 Creacion de Actions y Reducers

---
+++ src/Components/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h1>Prueba Tecnica React Apuesta Total</h1>
+      <h2>Carrito de Compras</h2>
+      <h3>Productos</h3>
+    </div>
+  );
+}
+
+export default ShoppingCart;
---
+++ src/Reducers/ShopReducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopReducer = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ShopReducer;
