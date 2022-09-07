f4577c7cc5f1d4a783db7243c140ae8c30d3eb07
ayrtoncu <bc.ayrton@gmail.com>
2022-09-07 18:27:55 -0500

    #6 Carrito de compras agregado

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,6 +1,7 @@
-import React,{useReducer,useState,useEffect} from 'react';
-import { ShopInitialState, shoppingReducers } from '../Reducers/ShopReducer';
+import React, { useReducer, useState, useEffect } from "react";
+import { ShopInitialState, shoppingReducers } from "../Reducers/ShopReducer";
 import { appfirebase } from "../Credenciales";
+import { css } from "@emotion/css";
 import {
   getFirestore,
   collection,
@@ -11,12 +12,16 @@ import {
   getDoc,
   setDoc,
 } from "firebase/firestore";
-import ProductItem from './ProductItem';
+import ProductItem from "./ProductItem";
+import CartItem from "./CartItem";
+import { TYPES } from "../Actions/ShopActions";
 
 const ShoppingCart = () => {
   const [state, dispatch] = useReducer(shoppingReducers, ShopInitialState);
   const db = getFirestore(appfirebase);
   const [lista, setLista] = useState([]);
+  // const [cart,setCart]=useState([]);
+  let cart=[]
   useEffect(() => {
     const getLista = async () => {
       try {
@@ -31,27 +36,51 @@ const ShoppingCart = () => {
       }
     };
     getLista();
-  },[]);
-  const { cart } = state;
+  }, []);
 
   const addToCart = (id) => {
-    console.log(id)
-  }
-  const delFromCart = () => { }
-  const clearCart = () => { }
+    let newitem=[]
+    lista.forEach((producto) => {
+      if (producto.id === id) {
+        newitem = producto;
+      }
+    })
+    // cart.push(newitem);
+    cart.push(newitem)
+    console.log(cart)
+  };
+  console.log(lista);
+
+  const delFromCart = () => {};
+  const clearCart = () => {};
 
   return (
     <div>
       <h1>Prueba Tecnica React Apuesta Total</h1>
       <h2>Carrito de Compras</h2>
+      <article>
+        <button onClick={clearCart}>Limpiar Carrito</button>
+
+          <CartItem data={cart} delFromCart={delFromCart} />
+
+      </article>
       <h3>Productos</h3>
-      <div>
+      <div className={box}>
         {lista.map((lista) => (
           <ProductItem key={lista.id} data={lista} addCart={addToCart} />
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default ShoppingCart;
+
+let box = css`
+  padding: 1rem;
+  margin: 1rem;
+  box-shadow: 0 0 1rem #00000025 inset;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1rem;
+`;
